3a040ffaed0fc1d5d0dbb128b6f365448d777c3c
Ryan Paglinawan <rymotion1@me.com>
2025-03-03 00:22:59 -0800

    updated support page script and text

--- src/components/donation/donation.jsx
+++ src/components/donation/donation.jsx
@@ -1,19 +1,22 @@
+import GofundmeWidget from "../gofundme";
+
 const DonationBody = () => {
   return (
     <div className="flex flex-row min-h-screen min-w-screen justify-center items-center h-full w-full">
       <div className="flex flex-col items-center px-4 p-20">
         <h1>Your Proceeds</h1>
-        <p className="py-20">
-          While we continue our capacity and program building for our
-          organization, your contribution helps support our efforts, test,
-          build, and sustain these "third-place" program. Every contribution
-          made we also ask contributors if they want regular updates on the
-          impact their proceeds made.
+        <p className="py-10">
+          While we continue our capacity and program efforts, your contribution
+          helps support our efforts, test, build, and sustain these
+          "third-place" program. Every contribution made we also ask
+          contributors if they want regular updates on the impact their proceeds
+          made.
         </p>
-        <p className="py-20">
-          Future efforts we look to support athletes and have event
-          sponsorships. More details about our Athletics Fund will be released
-          once we are ready.
+        <p className="py-10">
+          If you want to partner with us to help build a better, healthier, and
+          safer Inland Empire you can contribute to our General COntribution
+          Fund. Monthly we look to allocate 10% of the proceeds given to be used
+          on the other efforts listed below.
         </p>
         <div className="py-10">
           <script
@@ -25,6 +28,13 @@ const DonationBody = () => {
             src="https://widgets.givebutter.com/latest.umd.cjs?acct=ZJkY9KxBnEd5eqgh&p=other"
           ></script>
           <givebutter-widget id="LPDl8L"></givebutter-widget>
+        </div>
+        <p className="py-10">
+          If you are looking to make a direct impact with a specific effort we
+          are supporting here are the projects we are assisting with.
+        </p>
+        <div className="flex flex-col items-center padding-10">
+          <GofundmeWidget />
         </div>
       </div>
     </div>
