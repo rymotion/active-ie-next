4a166ca0dd0d140583df069b3db59453d1a0c346
Ryan Paglinawan <rymotion1@me.com>
2025-03-03 07:10:29 -0800

    updated linter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { Analytics } from "@vercel/analytics/react";
 import BMXJam from "../assets/events/8B731E4D-00A5-4211-B4E6-33A9902F0EC9.jpeg";
 import Sweatpals from "../assets/events/1452E6AA-867D-4D63-95DB-15597572C6E5.jpeg";
 import Image from "next/image";
+import React from "react";
 
 export default function Home() {
   return (
@@ -17,16 +18,26 @@ export default function Home() {
           <div className="flex flex-col items-center px-4 max-w-xl">
             <OrgContentBody />
           </div>
-          <div className="flex flex-row items-center padding-10">
+          <div className="flex flex-col items-center padding-10">
             <GofundmeWidget />
-            <Image
-              src={Sweatpals}
-              alt="BMX Bike Jam"
-              width="400"
-              height="400"
-              priority
-            ></Image>
+            <div className="flex flex-row items-center padding-10">
+              <Image
+                src={Sweatpals}
+                alt="BMX Bike Jam"
+                width="400"
+                height="400"
+                priority
+              ></Image>
+              <Image
+                src={BMXJam}
+                alt="BMX Bike Jam"
+                width="400"
+                height="400"
+                priority
+              ></Image>
+            </div>
           </div>
+
           <div className="flex flex-col items-center px-4 ptop-20">
             <h1>Our Long-Term Goal</h1>
             <p className="max-w-xl text-center">
