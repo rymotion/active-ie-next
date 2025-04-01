8511a62d857263dbc6d84fba89977e0c481477f4
Ryan Paglinawan <rymotion1@me.com>
2025-04-01 15:03:31 -0700

    upgrades to contact and newsletter registration

---
+++ src/app/contact/newsletter_subscribe.jsx
@@ -0,0 +1,18 @@
+const NewsletterWidget = () => {
+  return (
+    <div className="flex flex-row min-h-screen min-w-screen justify-center items-center h-full w-full">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSexLsoBAeGLWtr4A9rpTD_g9IwOtMfDCwciMFexgIn-9NMXMQ/viewform?embedded=true"
+        width="650"
+        height="500"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+      >
+        Loading…
+      </iframe>
+    </div>
+  );
+};
+
+export default NewsletterWidget;
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import Screen from "@/components/screen/screen";
 import InstaWidget from "./instagram_gallery";
+
+import NewsletterWidget from "./newsletter_subscribe";
 import { Analytics } from "@vercel/analytics/react";
 
 export default function Contact() {
@@ -8,8 +10,14 @@ export default function Contact() {
     <>
       <div>
         <Screen>
-          <div>
+          <div className="display: flex; flex-direction: column; align-items: center">
+            <NewsletterWidget />
             <InstaWidget />
+          </div>
+          <div className="flex flex-row min-h-screen min-w-screen justify-center items-center h-full w-full">
+            <h2>Inquries:</h2>
+
+            <p>organization@activeie.org</p>
           </div>
           <Analytics />
         </Screen>
