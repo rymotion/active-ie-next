40acaf4a70788f9f6198814ac9445fdcef0edb2d
Ryan Paglinawan <rymotion1@me.com>
2025-05-14 15:28:20 -0700

    adjusted rule set

--- src/components/screen/screen.tsx
+++ src/components/screen/screen.tsx
@@ -1,16 +1,17 @@
 "use client";
 import Navbar from "@/components/navbar";
 import DisclosureBar from "@/components/disclosure";
-import { motion, useScroll, useTransform } from "framer-motion";
+// import { motion, useScroll, useTransform } from "framer-motion";
 
+// export default function Screen({ children }: { children: React.ReactNode }) {
 export default function Screen({ children }: { children: React.ReactNode }) {
-  const { scrollYProgress } = useScroll();
-  const scaleValue = useTransform(scrollYProgress, [0, 1], [1, 2]);
-  const opacityValue = useTransform(scrollYProgress, [0, 0.5, 1], [1, 0.5, 0]);
+  // const { scrollYProgress } = useScroll();
+  // const scaleValue = useTransform(scrollYProgress, [0, 1], [1, 2]);
+  // const opacityValue = useTransform(scrollYProgress, [0, 0.5, 1], [1, 0.5, 0]);
 
   return (
     <>
-      <Navbar />{" "}
+      <Navbar />
       <div className="flex flex-row min-h-screen min-w-screen justify-center items-center h-full w-full">
         <main>{children}</main>
       </div>
--- src/components/sw-calendar/sweatpals-calendar.tsx
+++ src/components/sw-calendar/sweatpals-calendar.tsx
@@ -1,6 +1,3 @@
-import Link from "next/link";
-import Head from "next/head";
-
 const SweatpalsCalendar = () => {
   return {
     /* <Head>
