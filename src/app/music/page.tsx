53b8daff526db6a757944dbf0f616b57d768a7f7
Ryan Paglinawan <rymotion1@me.com>
2025-05-14 15:38:26 -0700

    removed music page

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -1,12 +1,12 @@
-export default function MusicPage() {
-  return (
-    <iframe
-      allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
-      frameborder="0"
-      height="450"
-      style="width:100%;max-width:660px;overflow:hidden;border-radius:10px;"
-      sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
-      src="https://embed.music.apple.com/us/playlist/run-club/pl.u-m6LBsyDPGAK"
-    ></iframe>
-  );
-}
+// export default function MusicPage() {
+//   return (
+//     <iframe
+//       allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+//       frameborder="0"
+//       height="450"
+//       style="width:100%;max-width:660px;overflow:hidden;border-radius:10px;"
+//       sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+//       src="https://embed.music.apple.com/us/playlist/run-club/pl.u-m6LBsyDPGAK"
+//     ></iframe>
+//   );
+// }
