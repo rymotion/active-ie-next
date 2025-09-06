88e7bf2a37d4414e5fc57b0ef7cbd6f75554925d
Ryan Paglinawan <rymotion1@me.com>
2025-09-06 06:15:08 -0700

    updated for Fontana ff

--- src/app/special-event/page.tsx
+++ src/app/special-event/page.tsx
@@ -13,6 +13,12 @@ import React, { useState } from "react";
 import ACTVDialog from "@/components/dialog/dialog";
 
 import SweatpalEvents from "@/components/events/sw_events";
+import VolunteerInterestWidget from "../volunteer/volunteer_interest";
+import GofundmeWidget from "@/components/gofundme";
+import { motion } from "framer-motion";
+import PublicCommentBoard from "../projects/public_comment_board";
+import ProjectFundingWidget from "../projects/bike_project_widget";
+import BikeProjectWindow from "../projects/bike_project_window";
 
 export default function SpecialEvent() {
   const [open, setOpen] = useState(false);
@@ -60,22 +66,78 @@ export default function SpecialEvent() {
       <Analytics />
       <Screen>
         <main>
-          <div
+          {/* <div
             className="flex flex-col justify-center px-5 w-full h-full"
             id="application-head"
           >
             <SweatpalEvents />
-          </div>
+          </div> */}
 
           <InstaWidgetMain />
           <InstaWidgetD2D />
-          <AltGFMPoster />
+          <section className="min-h-screen py-20 bg-gray-100">
+            <div className="container mx-auto px-4 text-center">
+              <h2 className="text-3xl font-bold mb-6">Join Our Community</h2>
+              <p className="text-xl mb-8">
+                Want to work our events or be a part of our organization?
+              </p>
+              <VolunteerInterestWidget />
+            </div>
+          </section>
+          <div className="flex-grow">
+            <div className="text-center mb-6 sm:mb-8 w-full">
+              <h1 className="text-2xl sm:text-3xl font-bold mb-2 sm:mb-4">
+                Projects
+              </h1>
+              <p className="text-base sm:text-lg text-gray-300 max-w-2xl mx-auto px-2">
+                These are our current ongoing projects and efforts across the
+                Inland Empire.
+              </p>
+            </div>
+            <motion.section
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.7, delay: 0.2 }}
+              className="w-full max-w-6xl mx-auto px-2 sm:px-0"
+            >
+              <h2 className="text-xl sm:text-2xl font-semibold text-center mb-6 sm:mb-8">
+                Bike Ramp Project Update
+              </h2>
+              <div className="w-full grid grid-cols-1 lg:grid-cols-12 gap-8">
+                <div className="lg:col-span-7 xl:col-span-8">
+                  <BikeProjectWindow />
+                </div>
+                <div className="lg:col-span-5 xl:col-span-4 flex items-center justify-center">
+                  <div className="w-full max-w-md">
+                    <ProjectFundingWidget />
+                  </div>
+                </div>
+                <div className="flex flex-col items-center justify-center mx-auto px-4">
+                  <h2 className="text-xl sm:text-2xl font-semibold text-center mb-6 sm:mb-8">
+                    Public Comment Board
+                  </h2>
+                  <p className="text-base sm:text-lg text-gray-300 max-w-2xl mx-auto px-2">
+                    By selecting the pup-out window option from the image below
+                    you will be taken to the file hosted on Google Drive to
+                    provide feedback on the bike ramp project. Have a voice in
+                    the planning of the project. You must be signed in to a
+                    registered Google account to participate.
+                  </p>
+                  <PublicCommentBoard />
+                </div>
+                <div className="lg:col-span-12 xl:col-span-12">
+                  <GofundmeWidget />
+                </div>
+              </div>
+            </motion.section>
+          </div>
+          {/* <AltGFMPoster /> */}
           <div className="flex justify-center">
             <button
               onClick={() => setOpen(true)}
               className="text-2xl font-bold bg-black text-white hover:bg-red-500 transition-all duration-300 transform hover:scale-105"
             >
-              Newsletter Subscribe
+              Click here to subscribe to our newsletter.
             </button>
           </div>
           <div className="flex flex-col min-h-screen min-w-screen justify-center items-center  w-full">
