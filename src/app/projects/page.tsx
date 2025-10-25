34274d23348365ee52206e14898dc051ca6f5991
Ryan Paglinawan <ryan.p@venhub.com>
2025-10-25 11:54:26 -0700

    updated project and landings screens

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,7 +4,7 @@ import { Analytics } from "@vercel/analytics/react";
 import ProjectFundingWidget from "./bike_project_widget.jsx";
 import { motion } from "framer-motion";
 import BikeProjectWindow from "@/app/projects/bike_project_window.jsx";
-import BikeParkForm from "@/app/volunteer/volunteer_interest.jsx";
+import BikeParkForm from "@/app/projects/bike park form_b.jsx";
 import GofundmeWidget from "@/components/gofundme.jsx";
 const Project = () => {
   return (
